639814b77d501bcf0b1a894c770f01fe9ba52340
pramaths <103276292+pramaths@users.noreply.github.com>
2024-04-21 02:54:15 +0530

    one

--- mysubscription/mysubscription/settings.py
+++ mysubscription/mysubscription/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-20jz521(kkp2i@k(c&_)-)d4-qj0p^pc4-=939buu-fh24hdu1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
